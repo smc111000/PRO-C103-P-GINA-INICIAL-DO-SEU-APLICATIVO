24cd4f18f2a55b1dd621dcf9ffe1402241e18674
Samuel GM <110042220+smc111000@users.noreply.github.com>
2023-10-15 11:31:57 -0300

    Add files via upload

---
+++ PRO_1-4_C103_TemplateDoProjeto-main/App.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import WelcomeScreen from './Screens/WelcomeScreen';
+import { AppTabNavigator } from './components/AppTabNavigator';
+
+
+export default class App extends React.Component{
+  render(){
+    return <AppContainer/>;   
+  } 
+}  
+ 
+const switchNavigator = createSwitchNavigator({
+  AppTabNavigator: { screen: AppTabNavigator },    
+});
+  
+const AppContainer = createAppContainer(switchNavigator);            
